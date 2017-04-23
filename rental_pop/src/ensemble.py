35156d47baad0de2ad543a6e1087c0ea8a99122c
Reuben Li <mesolithic@gmail.com>
2017-04-23 10:01:47 +0000

    Add normalization (prior)

--- rental_pop/src/ensemble.py
+++ rental_pop/src/ensemble.py
@@ -8,6 +8,47 @@ import pandas as pd
 PATH = '../output/ensemble/'
 MODELS = glob.glob(PATH + '*.csv')
 
+interest_levels = ['low', 'medium', 'high']
+
+tau_test = {
+    'low': 0.69195995, 
+    'medium': 0.23108864,
+    'high': 0.07695141, 
+}
+
+def correct(df, train=True, verbose=False):
+    if train:
+        tau = tau_train
+    else:
+        tau = tau_test
+        
+    df_sum = df[interest_levels].sum(axis=1)
+    df_correct = df[interest_levels].copy()
+    
+    if verbose:
+        y = df_correct.mean()
+        a = [tau[k] / y[k]  for k in interest_levels]
+        print( a)
+    
+    for c in interest_levels:
+        df_correct[c] /= df_sum
+
+    for i in range(20):
+        for c in interest_levels:
+            df_correct[c] *= tau[c] / df_correct[c].mean()
+
+        df_sum = df_correct.sum(axis=1)
+
+        for c in interest_levels:
+            df_correct[c] /= df_sum
+    
+    if verbose:
+        y = df_correct.mean()
+        a = [tau[k] / y[k]  for k in interest_levels]
+        print( a)
+
+    return df_correct
+
 def create_ensemble():
     """ simple averaging ensemble """
     model_list = {}
@@ -18,6 +59,7 @@ def create_ensemble():
     ensem = allm.groupby(level=0).mean()
     timestamp = str(int(time.time()))
     ensem['listing_id'] = [int(i) for i in ensem['listing_id']]
+    ensem = correct(ensem, train=False, verbose=True)
     ensem.to_csv(PATH + 'out/' + 'ensemble_' + timestamp + '.csv', index=False)
     return
 
